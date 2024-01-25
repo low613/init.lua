4d25e2a72819e3814eba819fe6cc3e7d128d25f6
Eli Lowinger <low613@gmail.com>
2024-01-25 11:32:38 +1100

    more fixes

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,7 +1,7 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
--- See the kickstart.nvim README for more information
+-- See the ki€þXkbkstart.nvim README for more information
 return {
 	{
 		'nvim-tree/nvim-web-devicons',
@@ -31,4 +31,46 @@ return {
 		end,
 	},
 	{ "AndreM222/copilot-lualine" },
+	{
+		"theprimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("harpoon"):setup({
+				global_settings = {
+					save_on_toggle = false,
+				},
+			})
+		end,
+		keys = {
+			{ "<leader>ha", function() require("harpoon"):list():append() end,  desc = "harpoon file", },
+			{
+				"<C-e>",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "harpoon quick menu",
+			},
+			{ "<leader>1",  function() require("harpoon"):list():select(1) end, desc = "harpoon to file 1", },
+			{ "<leader>2",  function() require("harpoon"):list():select(2) end, desc = "harpoon to file 2", },
+			{ "<leader>3",  function() require("harpoon"):list():select(3) end, desc = "harpoon to file 3", },
+			{ "<leader>4",  function() require("harpoon"):list():select(4) end, desc = "harpoon to file 4", },
+			{ "<leader>5",  function() require("harpoon"):list():select(5) end, desc = "harpoon to file 5", },
+		},
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup()
+		end
+	},
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup()
+		end
+	}
 }
